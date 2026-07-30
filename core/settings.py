a23f400482534ba382265917e6c8e747185135c8
Ewelinaszymanska85 <27kwiecien1985@gmail.com>
2026-07-30 10:40:00 +0200

    Przygotowanie produkcyjne: Gunicorn zamiast runserver, konfiguracja przez zmienne srodowiskowe (SECRET_KEY, DEBUG, ALLOWED_HOSTS)

--- core/settings.py
+++ core/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cf3g-7io8^q%tlbe31@oy(w-i5epudr9713$*8isr@l8nchxhc'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cf3g-7io8^q%tlbe31@oy(w-i5epudr9713$*8isr@l8nchxhc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'  
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') 
 
 
 # Application definition
